d0567eb7d34f446a45cf523bc3567fa5d3166736
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-04-03 21:41:11 +0100

    Create Program to call for methods in classes

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 
 namespace BetterColorSpheres
 {
@@ -6,7 +6,28 @@ namespace BetterColorSpheres
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Color redColor = new Color(255, 0, 0);
+            Color blueColor = new Color(0, 0, 255);
+            Color greenColor = new Color(0, 255, 0);
+
+            Sphere sphere1 = new Sphere(redColor, 5);
+            Sphere sphere2 = new Sphere(blueColor, 10);
+            Sphere sphere3 = new Sphere(greenColor, 7);
+
+            sphere1.Throw();
+            sphere1.Throw();
+            sphere2.Throw();
+            sphere3.Throw();
+
+            sphere2.Pop();
+
+            Console.WriteLine(sphere1);
+            Console.WriteLine(sphere2);
+            Console.WriteLine(sphere3);
+
+            Console.WriteLine($"Red sphere grey level: {sphere1.Color.Grey}");
+            Console.WriteLine($"Blue sphere grey level: {sphere2.Color.Grey}");
+            Console.WriteLine($"Green sphere grey level: {sphere3.Color.Grey}");
         }
     }
 }
