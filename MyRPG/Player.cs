b66120a32f598cc70fd5c6bd605a06c1df572fe8
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-04-04 14:55:35 +0100

    Create properties Health and MaxHealth

--- MyRPG/Player.cs
+++ MyRPG/Player.cs
@@ -27,6 +27,27 @@ namespace Player
             return 1 + XP /1000;
         }
        }
-       
+       public float Health
+       {
+        get => health;
+        private set
+        {
+            health = value;
+            if (health < 0 )
+                health = 0;
+            if (health > MaxHealth)
+                health = MaxHealth;
+
+        }
+       }
+       public float MaxHealth
+        {
+            get
+            {
+                return 100 + (Level-1)*20
+            }
+        }
+
+        
     }
 }
