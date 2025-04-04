313ce0819f95f004f5b8b908f71cc1c343c212c8
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-04-04 11:49:11 +0100

    Create Player class and HighScore and WinRate methods

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,33 @@
+﻿namespace PlayerStats
+{
+    public class Player
+    {
+        private float _highScore;
+        private int _playedGames;
+        private int _wonGames;
+
+        public float HighScore
+        {
+            get => _highScore;
+            set
+            {
+                if (value > _highScore)
+                {
+                    _highScore = value;
+                }
+            }
+        }
+
+        public string Name { get; }
+
+        public float WinRate
+        {
+            get
+            {
+                if (_playedGames == 0)
+                {
+                    return 0;
+                }
+                return (float)_wonGames / _playedGames;
+            }
+        }
